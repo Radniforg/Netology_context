8abe93e3658aa60648fdf69533310b541b554520
Ivan Alexandrov <radniforg@yandex.ru>
2020-04-10 16:14:17 +0300

    working .py file added

--- runtime.py
+++ runtime.py
@@ -1,5 +1,7 @@
 import datetime
 import sys
+import json
+from pprint import pprint
 from contextlib import contextmanager
 
 def runtime(start_time, finish_time):
@@ -9,19 +11,30 @@ def runtime(start_time, finish_time):
     print(f'Время окончания работы кода: {finish_time}')
     print(f'Времени потрачено на выполнение кода: {script_runtime[0]} часов, {script_runtime[1]} минут, {script_runtime[2]} секунд')
 
+def decompose(deser_json):
+    decomposed_json = []
+    for hit in deser_json:
+        temporary_dictionary = {'number': hit['num'], 'strain': hit['description'][0]['title'],
+                                'len': hit['hsps'][0]['align_len'],
+                                'gaps': hit['hsps'][0]['gaps'], 'sequence': hit['hsps'][0]['hseq']}
+        decomposed_json.append(temporary_dictionary)
+    return decomposed_json
+
 @contextmanager
-def decompose(path, encoding='utf-8'):
+def timer(path, encoding='utf-8'):
     try:
         function_initiation = datetime.datetime.now()
-        xml = open(path, encoding = encoding)
-        yield xml
+        json_file = open(path, encoding = encoding)
+        yield json_file
     finally:
         exc_type, exc_val, exc_tb = sys.exc_info()
         if exc_type is not None:
             print(f'Внимание! Ошибка: {exc_val}')
-        xml.close()
+        json_file.close()
         function_finished = datetime.datetime.now()
         runtime(function_initiation, function_finished)
 
-with decompose('gene.json') as gene_file:
-    pass
+
+with timer('gene.json') as gene_file:
+    alignments = json.load(gene_file)
+    pprint(decompose(alignments['BlastOutput2'][0]['report']['results']['search']['hits']))
